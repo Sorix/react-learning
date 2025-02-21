f7cfd6223eb353bcd28ed34725b5a76af324a808
Vasily Ulianov <vasily@me.com>
2025-02-21 22:11:17 +0300

    Part 2-11

--- part2-10/src/App.jsx
+++ part2-10/src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Notification from './components/Notification'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456', id: 1, isShown: true },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2, isShown: true },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 3, isShown: true },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4, isShown: true }
-	])
+	const [persons, setPersons] = useState([])
 	const [duplicateNotification, setDuplicateNotification] = useState({
 		isShown: false,
 		name: null,
 	})
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:3001/persons")
+			.then(response => {
+				console.log("Data received")
+				setPersons(response.data)
+			})
+	}, [])
 
 	return (
 		<div>
